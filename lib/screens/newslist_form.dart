93c931dee29f5e16026621499ef7b6b495ba1add
w7ldan <wildanhidayatirl@gmail.com>
2025-11-05 14:43:55 +0700

    Tutorial 7 Done

---
+++ lib/screens/newslist_form.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+import 'package:football_news/widgets/left_drawer.dart';
+
+class NewsFormPage extends StatefulWidget {
+    const NewsFormPage({super.key});
+
+    @override
+    State<NewsFormPage> createState() => _NewsFormPageState();
+}
+
+class _NewsFormPageState extends State<NewsFormPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _title = "";
+  String _content = "";
+  String _category = "update"; // default
+  String _thumbnail = "";
+  bool _isFeatured = false; // default
+
+  final List<String> _categories = [
+    'transfer',
+    'update',
+    'exclusive',
+    'match',
+    'rumor',
+    'analysis',
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text(
+              'Form Tambah Berita',
+            ),
+          ),
+          backgroundColor: Colors.indigo,
+          foregroundColor: Colors.white,
+        ),
+        drawer: LeftDrawer(),
+        body: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:[
+                // === Title ===
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Judul Berita",
+                      labelText: "Judul Berita",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                    onChanged: (String? value) {
+                      setState(() {
+                        _title = value!;
+                      });
+                    },
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return "Judul tidak boleh kosong!";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                      hintText: "Isi Berita",
+                      labelText: "Isi Berita",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                    onChanged: (String? value) {
+                      setState(() {
+                        _content = value!;
+                      });
+                    },
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return "Isi berita tidak boleh kosong!";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                // === Category ===
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DropdownButtonFormField<String>(
+                    decoration: InputDecoration(
+                      labelText: "Kategori",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                    initialValue: _category,
+                    items: _categories
+                        .map((cat) => DropdownMenuItem(
+                              value: cat,
+                              child: Text(
+                                  cat[0].toUpperCase() + cat.substring(1)),
+                            ))
+                        .toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _category = newValue!;
+                      });
+                    },
+                  ),
+                ),
+
+                // === Thumbnail URL ===
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "URL Thumbnail (opsional)",
+                      labelText: "URL Thumbnail",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                    onChanged: (String? value) {
+                      setState(() {
+                        _thumbnail = value!;
+                      });
+                    },
+                  ),
+                ),
+
+                // === Is Featured ===
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SwitchListTile(
+                    title: const Text("Tandai sebagai Berita Unggulan"),
+                    value: _isFeatured,
+                    onChanged: (bool value) {
+                      setState(() {
+                        _isFeatured = value;
+                      });
+                    },
+                  ),
+                ),
+                 Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            WidgetStateProperty.all(Colors.indigo),
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: const Text('Produk berhasil tersimpan'),
+                                content: SingleChildScrollView(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text('Judul: $_title'),
+                                      Text('Content: $_content'),
+                                      Text('Category: $_category'),
+                                      // TODO: Munculkan value-value lainnya
+                                      //   String _title = "";
+                                      // String _content = "";
+                                      // String _category = "update"; // default
+                                      // String _thumbnail = "";
+                                      // bool _isFeatured = false; // default
+                                    ],
+                                  ),
+                                ),
+                                actions: [
+                                  TextButton(
+                                    child: const Text('OK'),
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        _formKey.currentState!.reset();
+                        }
+                      },
+                      child: const Text(
+                        "Save",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+          )
+        ),
+        )
+      );
+  }
+}
---
+++ lib/widgets/left_drawer.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:football_news/screens/menu.dart';
+import 'package:football_news/screens/newslist_form.dart';
+
+class LeftDrawer extends StatelessWidget {
+  const LeftDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'Football News',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Text(
+                  "Seluruh berita sepak bola terkini di sini!",
+                  // TODO: Tambahkan gaya teks dengan center alignment, font ukuran 15, warna putih, dan weight biasa
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal,
+                  ),
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Home'),
+            // Bagian redirection ke MyHomePage
+            onTap: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MyHomePage(),
+                  ));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.post_add),
+            title: const Text('Add News'),
+            // Bagian redirection ke NewsFormPage
+            onTap: () {
+              /*
+              TODO: Buatlah routing ke NewsFormPage di sini,
+              setelah halaman NewsFormPage sudah dibuat.
+              */
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const NewsFormPage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.newspaper),
+            title: const Text('See Football News'),
+            onTap: () {
+              Navigator.pop(context); 
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Navigasi ke Halaman List Berita)'))
+              );
+            },
+          ),
+          // TODO: Buat ListTile baru untuk ke halaman melihat news
+        ],
+      ),
+    );
+  }
+}
